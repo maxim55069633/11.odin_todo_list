62209e240ac10a46182fa88daa481f18d119a411
maxim55069633 <1246003314@qq.com>
2022-11-01 22:11:35 +0800

    Everything is finished except the sort_by_date

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
 
+
 class Todo{
-    constructor(title, start_date, due_date, status, priority, description ){
+    constructor(title, start_date, due_date, status, priority, detail ){
         this.title=title;
         this.start_date=start_date;
         this.due_date=due_date;
         this.status=status;
         this.priority=priority;
-        this.description=description;
+        this.detail=detail;
     }
 
     static create_default_todo () {
         const sample =[new Todo("Sample", new Date().toISOString().slice(0, 10), new Date().toISOString().slice(0, 10), "Uncompleted", "1", "To be continued" )];
-        console.log(sample);
+        
         return sample;
     }
 
@@ -24,7 +25,7 @@ class Todo{
         const target_project =target_todo.parentNode.parentNode.parentNode.parentNode.parentNode;
         const target_project_index = target_project.current_project_index;
         
-        const edit_todo_buttons = document.querySelectorAll(`.modal-body>form`);
+        const edit_todo_buttons = document.querySelectorAll(`.modal-body>form[class="edit_todo_form"]`);
        
         edit_todo_buttons.forEach(
             item=>{
@@ -33,9 +34,6 @@ class Todo{
                     const item_project =target_todo.parentNode.parentNode.parentNode.parentNode.parentNode;
                     const item_project_index = item_project.current_project_index;
        
-
-                    // console.log(item_project_index + item_todo_index);
-                    console.log(item);
 
                     item.addEventListener("submit", 
                         (e) => {
@@ -47,7 +45,7 @@ class Todo{
                             formData.get("edit_todo_due_date"),
                             formData.get("edit_todo_status"),
                             formData.get("edit_todo_priority"),
-                            formData.get("edit_todo_description")
+                            formData.get("edit_todo_detail")
                             );
 
                             Project.users_project_book[item_project_index]["todo_lists"].splice(item_todo_index, 1, newly_edited_todo);
@@ -59,6 +57,87 @@ class Todo{
         );
     }
 
+
+
+    static display_todo_section(){
+        const accordion_sections = document.querySelectorAll(`div[class="accordion-collapse collapse"]`);
+        accordion_sections.forEach( 
+            (item, index) =>{
+
+
+                const todo_section =document.createElement("div");
+                todo_section.innerHTML= `
+                    <h2>Todos for Project ${index+1}</h2>
+                    <div class="sort_section">
+                        <label for="sorted_by">Sorted by: </label>
+                        <select name="sorted_by" id="sorted_by_${index}" required>
+                            <option value="title">Title</option>
+                            <option value="status">Status</option>
+                            <option value="priority">Priority</option>
+                            <option value="due_date">Due Date</option>
+                        </select>
+                    </div>
+                    <div class="display_todo_list_${index}"></div>
+                    `;
+                todo_section.classList.add("todo_section");
+                todo_section.current_project_id = index;
+                item.appendChild(todo_section);
+
+            }
+         )
+
+        Project.users_project_book.forEach( 
+            (item, index)=>{
+                Todo.display_todo_lists(item, index);
+            }
+         );
+
+         accordion_sections.forEach( 
+            (item, index) =>{
+                const sort_change_listener =document.querySelector(`#sorted_by_${index}`);
+                sort_change_listener.addEventListener("change", Todo.change_the_todo_section
+                 );
+            }
+         )
+    }
+
+    static sorted_by_title(a,b){
+        if ( a["title"] <= b["title"] )
+            return -1;
+        else if ( a["title"] > b["title"] )
+            return 1;
+    }
+
+    static sorted_by_status(a,b){
+        if ( a["status"] <= b["status"] )
+            return -1;
+        else if ( a["status"] > b["status"] )
+            return 1;
+    }
+
+    static sorted_by_priority(a,b){
+        if (a["priority"] <= b["priority"]) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    static  change_the_todo_section(e){
+        const current_project_index = e.target.parentNode.parentNode.current_project_id;
+        const sort_by = e.target.value;
+        if (sort_by === 'status'){
+            Project.users_project_book[current_project_index]["todo_lists"].sort( Todo.sorted_by_status);
+        } else if (sort_by === 'priority' ) {
+            Project.users_project_book[current_project_index]["todo_lists"].sort( Todo.sorted_by_priority);
+        } else if (sort_by === 'title') {
+            Project.users_project_book[current_project_index]["todo_lists"].sort( Todo.sorted_by_title);
+        }
+
+        Todo.display_todo_section();
+    }
+    
+
     static delete_todo(e){
         
         const target_todo = e.target.parentNode.parentNode.parentNode;
@@ -69,12 +148,13 @@ class Todo{
 
         Project.users_project_book[target_project_index]["todo_lists"].splice(target_todo_index, 1);
         Project.store_projects_locally();
-        Project.display_projects();
+        Todo.display_todo_section();
         
     }
 
     static display_todo_lists( current_project, project_index ) {
             const display_todo_list = document.querySelector(`.display_todo_list_${project_index}`);
+            display_todo_list.innerHTML="";
         
             current_project["todo_lists"].forEach( (todo_item, todo_index) =>{
                 const todo_pair = document.createElement("div");
@@ -91,7 +171,7 @@ class Todo{
                             
                                 <div class="modal-body">
                                 
-                                    <form action="#" method="post" id="edit_project${project_index}_todo${todo_index}">
+                                    <form action="#" class="edit_todo_form" method="post" id="edit_project${project_index}_todo${todo_index}">
                                     <fieldset>
                                     <legend class="edit_todo_legend">Edit this todo:</legend>
                                     <div class="edit_todo_flex_area">
@@ -129,13 +209,10 @@ class Todo{
                                         </div>
 
                                         <div class="edit_todo_row">
-                                            <label for="edit_todo_description">Description: </label>
-                                            <textarea name="edit_todo_description" cols="30" rows="3" maxlength="1500"></textarea>
+                                            <label for="edit_todo_detail">Detail: </label>
+                                            <textarea name="edit_todo_detail" cols="30" rows="3" maxlength="1500"></textarea>
                                         </div>
                                     </div>
-                        
-                                        
-                                        
                                     </fieldset>
                                     </form>
 
@@ -172,8 +249,8 @@ class Todo{
 
                     const todo_control_icon = document.createElement("span");
                     todo_control_icon.innerHTML =
-                        `<span class="todo_view_icon"><span>VIEW</span></span>
-                        <span class="todo_edit_icon" data-bs-toggle="modal" data-bs-target="#todo_modal_project${project_index}_${todo_index}" ><span>EDIT</span> </span>
+                        `<span class="todo_edit_icon" data-bs-toggle="modal" data-bs-target="#todo_modal_project${project_index}_${todo_index}" ><span>EDIT</span> </span>
+                        <span class="todo_detail_icon" id="todo_detail_project${project_index}_${todo_index}" data-bs-toggle="tooltip" data-bs-placement="right" title="Tooltip on right" ><span>DETAIL</span> </span>
                         <span class="todo_del_icon"><span>DELETE</span></span>`;
                     todo_control_icon.classList.add("todo_general_pair");
                     todo_pair.appendChild(todo_control_icon);
@@ -181,9 +258,10 @@ class Todo{
                 todo_pair.classList.add("todo_general");
                 todo_pair.current_todo_index = todo_index;
                 
-                
             display_todo_list.appendChild(todo_pair);
-            
+
+            const todo_detail_hidden_content = document.querySelector(`#todo_detail_project${project_index}_${todo_index}`);
+            todo_detail_hidden_content.setAttribute("title", Project.users_project_book[project_index]["todo_lists"][todo_index]["detail"]);
             } 
          );
 
@@ -344,12 +422,43 @@ class Project{
                     Project.users_project_book[index_of_project_to_be_edited]["todo_lists"]
                     );
                     
-                    console.log(index_of_project_to_be_edited);
                     Project.users_project_book.splice(index_of_project_to_be_edited,1,newly_edited_project);
                     Project.store_projects_locally();
                 }
             );  
            }
+        );
+    }
+
+    static add_todo_for_current_project(){
+
+        const add_todo_form_listeners= document.querySelectorAll(`form[class="add_todo_form"]`);
+
+        add_todo_form_listeners.forEach(
+            item=>{
+
+                item.addEventListener("submit", (e)=>{
+
+
+                    const target_project = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode
+                    const target_project_index = target_project.current_project_index;
+
+                    const formData = new FormData(e.target);
+
+                    const newly_added_todo=new Todo(
+                    formData.get("add_todo_title"),
+                    formData.get("add_todo_start_date"),
+                    formData.get("add_todo_due_date"),
+                    formData.get("add_todo_status"),
+                    formData.get("add_todo_priority"),
+                    formData.get("add_todo_detail")
+                    );
+                    
+                    
+                    Project.users_project_book[target_project_index]["todo_lists"].push(newly_added_todo);
+                    Project.store_projects_locally();
+                } )
+            }
         );
     }
 
@@ -381,8 +490,7 @@ class Project{
 
                 const item_control_icon = document.createElement("span");
                 item_control_icon.innerHTML =
-                `<span class="project_view_icon"><span>VIEW</span></span>
-                <span class="project_edit_icon collapsed"  type="button" data-bs-toggle="collapse" data-bs-target="#collapse_${index}" aria-expanded="true" aria-controls="collapse_${index}"><span>EDIT</span> <svg class="rotation" style="width:24px;height:24px" viewBox="0 0 24 24">
+                `<span class="project_edit_icon collapsed"  type="button" data-bs-toggle="collapse" data-bs-target="#collapse_${index}" aria-expanded="true" aria-controls="collapse_${index}"><span>EDIT</span> <svg class="rotation" style="width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M1,21H23L12,2" />
             </svg></span>
                 <span class="project_del_icon"><span>DELETE</span></span>`;
@@ -432,19 +540,82 @@ class Project{
                             Save changes
                         </button>
 
-                        <button type="button" class="project_add_todo">
+                        <button type="button" class="project_add_todo" data-bs-toggle="modal" data-bs-target="#add_todo_modal_project${index}">
                             Add a todo
                         </button>
+
+                     
+
                     </div>
                     </fieldset>
                     </form>
 
-                    <div class="todo_section">
-                        <h2>Todos for project ${index+1}</h2>
-                        <div class="display_todo_list_${index}"></div>
+
+                    <div class="modal fade" id="add_todo_modal_project${index}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="add_todo_modal_project${index}_Label" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="add_todo_modal_project${index}_Label">Add a Todo for Project ${index+1}</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+
+                            <form action="#" class="add_todo_form" method="post" id="add_todo_for_project${index}">
+                                <fieldset>
+                                <div class="add_todo_flex_area">
+                                    <div class="add_todo_row">
+                                        <label for="add_todo_title">Todo Title: </label>
+                                        <input type="text" name="add_todo_title" id="add_todo_title_for_project${index}" required>    
+                                    </div>
+
+                                    <div class="add_todo_row">
+                                        <label for="add_todo_start_date">Todo Start Date: </label>
+                                        <input type="date" name="add_todo_start_date" id="add_todo_start_date_for_project${index}" required>    
+                                    </div>
+
+                                    <div class="add_todo_row">
+                                        <label for="add_todo_due_date">Todo Due Date: </label>
+                                        <input type="date" name="add_todo_due_date" id="add_todo_due_date_for_project${index}" required>    
+                                    </div>
+
+                                    <div class="add_todo_row">
+                                        <label for="add_todo_status">Status: </label>
+                                        <select name="add_todo_status" id="add_todo_status" required>
+                                            <option value="Completed">Completed</option>
+                                            <option value="Uncompleted">Uncompleted</option>
+                                        </select>
+                                    </div>
+                                    
+                                    <div class="add_todo_row">
+                                        <label for="add_todo_priority">Priority: </label>
+                                        <select name="add_todo_priority" id="add_todo_priority" required>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="add_todo_row">
+                                        <label for="add_todo_detail">Detail: </label>
+                                        <textarea name="add_todo_detail" cols="30" rows="3" maxlength="1500"></textarea>
+                                    </div>
+                                </div>
+                                </fieldset>
+                            </form>
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary" form="add_todo_for_project${index}">Confirm</button>
+                            </div>
+                            </div>
+                        </div>
                     </div>
+
                     
                 </div>`;
+
                 project_item.appendChild(project_edit);
                 project_edit.classList.add("project_edit");
 
@@ -452,16 +623,19 @@ class Project{
             project_list.appendChild(project_item);
             project_item.current_project_index= index;
 
-            Todo.display_todo_lists(item, index);
 
             // Setting placeholder as an attribute in the input won't keep the spaces 
             document.getElementById(`edit_project_title_${index}`).placeholder =item["title"];
 
+
         });
+
+        Todo.display_todo_section();
 
         const delete_buttons = document.querySelectorAll(".project_del_icon");
         delete_buttons.forEach(item => item.addEventListener("click", this.delete_project) );
         this.edit_project();
+        this.add_todo_for_current_project();
     }
 }
 
